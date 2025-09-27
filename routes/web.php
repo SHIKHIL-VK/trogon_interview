d02f7c796ae02e286720d7836d589bda3895c8de
SHIKHIL-VK <shikhilkolappa@gmail.com>
2025-09-27 10:50:18 +0530

    initiol commit

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', [UserController::class, 'login']);
+Route::get('/user', [UserController::class, 'index']);
